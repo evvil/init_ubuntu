dfc68b0c00291382ccb99be78009fac8ad5714e8
Evvil <voiceality@gmail.com>
2024-04-28 13:29:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
-sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-add-repository ppa:fish-shell/release-3 -y
 sudo apt update -y
-sudo apt install fish
+sudo apt install fish -y
+chsh -s /usr/bin/fish
